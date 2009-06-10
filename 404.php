54ed7da242998915db33b1430c9b4ff9b0ea3056
Evandro Pastor <evandro@nomad.local>
2009-06-10 16:25:35 -0300

    Fix 404 image and remove text.

--- 404.php
+++ 404.php
@@ -1,31 +1,31 @@
 <?php get_header();?>
-
-		
+	
 	<div id="page" class="grid_12">
-	<img src="<?php bloginfo('template_directory'); ?>/img/foresight_404_700x567.png" alt="image" width="700" height="567" alt="404 error" />
+	<img src="<?php bloginfo('template_directory'); ?>/img/foresight_404_700x270.png" alt="image" width="700" height="270" alt="404 error" />
+	
+	
 	
 	<div id="error_404">
-	<h2>Error 404 - Not Found</h2>
-	
-	</div>
-	</div><!--end page-->
 		
-		<div class="grid_4" id="sidebar">
-			<?php /*if (is_page("about"))
-					dynamic_sidebar('about');
-				elseif (is_page('get-foresight'))
-					dynamic_sidebar('get-foresight');
-				elseif (is_page('community'))
-					dynamic_sidebar('community');
-				elseif (is_page('news'))
-					dynamic_sidebar('news');
-				elseif (is_page('contact'))
-					dynamic_sidebar('contact');
-				elseif (is_page('get-help'))
-					dynamic_sidebar('get-help');
-				else
-					get_sidebar();*/?>
-					
+	 <?php query_posts('pagename=404');    
+	global $more;
+	// set $more to 0 in order to only get the first part of the post
+	$more = 0; 
+	
+	// the Loop
+	while (have_posts()) : the_post(); 
+	  // the content of the post
+	  the_content('Read the full post »'); 
+	endwhile;
+	?>
+
+
+	</div><!--end error_404-->
+			
+			<img src="<?php bloginfo('template_directory'); ?>/img/foresight_404_arrow_699x265.png" alt="image" width="699" height="265" />
+			</div><!--end page-->
+		
+		<div class="grid_4" id="sidebar">	
 					<?php get_sidebar();?>
 		</div><!--end sidebar2-->
 
