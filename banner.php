323fde0c7ea72338297cc767e071f48c694e3aba
Alexandre Franke <alexandre.franke@gmail.com>
2009-08-21 18:38:15 +0200

    Fixed link to download page

--- banner.php
+++ banner.php
@@ -3,7 +3,7 @@
 		 <a href="<?php bloginfo('url');?>/five-reasons-to-use-foresight-linux/"><img src="<?php bloginfo('template_directory'); ?>/img/foresight_linux_five_reasons_banner_1020x321.png" alt="image" width="1020" height="321" class="main_banner"/></a>	
 
 <?php } else {?>
-		<a href="<?php bloginfo('url');?>/get-foresight-linux/"><img src="<?php bloginfo('template_directory'); ?>/img/foresight_linux_get_foresight_banner_940x150.jpg" alt="image" width="940" height="150" class="internal_banner"/></a>
+		<a href="<?php bloginfo('url');?>/download/"><img src="<?php bloginfo('template_directory'); ?>/img/foresight_linux_get_foresight_banner_940x150.jpg" alt="image" width="940" height="150" class="internal_banner"/></a>
 	<?php } ?>
 </div><!--end banner-->
 <div class="clear"> </div>
